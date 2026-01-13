b7468197c99a7c53e9dbed8d044e6fa8508e7b16
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 11:07:07 +0530

    Add files via upload

---
+++ evennumber.c
@@ -0,0 +1,12 @@
+//print even number 
+#include<stdio.h>
+int main(){
+    int n=22;
+    if(n%2==0){
+        printf("number is even");
+    }
+    else{
+        printf("number is odd");
+    }
+    return 0;
+}
